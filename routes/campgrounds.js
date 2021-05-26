365811aabc5b3335c20eeddfe22ef486c49b0861
JCEleanor <crcky4826@gmail.com>
2021-05-26 12:29:24 +0800

    set up success/error flash messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -44,7 +44,7 @@ router.post(
 	catchAsync(async (req, res) => {
 		const campground = new Campground(req.body.campground)
 		await campground.save()
-		req.flash('success', 'Successfully add a new campground')
+		req.flash('success', 'Successfully added a new campground')
 		res.redirect(`campgrounds/${campground._id}`)
 	})
 )
@@ -54,7 +54,10 @@ router.get(
 	'/:id',
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id).populate('reviews')
-		// console.log(campground);
+		if (!campground) {
+			req.flash('error', 'campground does not exist')
+			return res.redirect('/campgrounds')
+		}
 		res.render('campgrounds/show', { campground })
 	})
 )
@@ -64,17 +67,22 @@ router.get(
 	'/:id/edit',
 	catchAsync(async (req, res) => {
 		const campground = await Campground.findById(req.params.id)
+		if (!campground) {
+			req.flash('error', 'campground does not exist')
+			return res.redirect('/campgrounds')
+		}
 		res.render('campgrounds/edit', { campground })
 	})
 )
 
-//edit campground
+//update campground
 router.put(
 	'/:id',
 	validateCampground,
 	catchAsync(async (req, res) => {
 		const { id } = req.params
 		const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+		req.flash('success', 'Successfully updated a campground')
 		res.redirect(`/campgrounds/${campground._id}`)
 		// not "campground/${campground._id"
 	})
@@ -86,6 +94,7 @@ router.delete(
 	catchAsync(async (req, res) => {
 		const { id } = req.params
 		await Campground.findByIdAndDelete(id)
+		req.flash('success', 'Successfully deleted a campground')
 		res.redirect('/campgrounds')
 	})
 )
--- routes/reviews.js
+++ routes/reviews.js
@@ -34,6 +34,7 @@ router.post(
 		campground.reviews.push(review)
 		await review.save()
 		await campground.save()
+		req.flash('success', 'Successfully leave a new review')
 		res.redirect(`/campgrounds/${campground._id}`)
 	})
 )
@@ -45,6 +46,8 @@ router.delete(
 		const { id, reviewId } = req.params
 		await Campground.findByIdAndUpdate(id, { $pull: { review: reviewId } })
 		await Review.findByIdAndDelete(reviewId)
+		req.flash('success', 'Successfully deleted a review')
+
 		res.redirect(`/campgrounds/${id}`)
 	})
 )
