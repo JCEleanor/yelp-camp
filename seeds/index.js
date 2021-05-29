8122884397cd19978309f41db5c0e8607dd1e899
JCEleanor <crcky4826@gmail.com>
2021-05-29 22:39:11 +0800

    reset database & add author

--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
 	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
 		const camp = new Campground({
+			author: '60b0c1b771f54252dee25393',
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
 			image: 'https://source.unsplash.com/collection/483251',
